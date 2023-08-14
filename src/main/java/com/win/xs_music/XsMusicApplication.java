a453ead55900bbefe6f19ea942020930134a7bdb
mikudd3 <488607948@qq.com>
2023-08-14 15:41:37 +0800

    配置文件搭建

---
+++ src/main/java/com/win/xs_music/XsMusicApplication.java
@@ -0,0 +1,13 @@
+package com.win.xs_music;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XsMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XsMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/win/xs_music/XsMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.win.xs_music;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XsMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
