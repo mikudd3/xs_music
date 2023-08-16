d366d42a005674e559bcf269d03f39167d5acbfe
cbj <2789531926@qq.com>
2023-08-16 17:18:50 +0800

    歌单页面

---
+++ src/main/resources/client/js/song_list.js
@@ -0,0 +1,35 @@
+Vue.createApp({
+    data(){
+        return {
+            song_list: [],
+            title:"",
+            pic:"",
+            introduction:"",
+            type:"",
+            style_ids:"",
+            create_time:""
+        }
+    },
+    methods:{
+        listUsers(){
+            axios({
+                method:"get",
+                url:"#",
+                params:{
+                    title:this.title,
+                    pic:this.pic,
+                    introduction:this.introduction,
+                    type:this.type,
+                    style_ids:this.style_ids,
+                    create_time: this.create_time
+                }
+            }).then(res => {
+                this.song_list = res.song_list;
+            })
+        },
+
+    },
+    mounted(){
+        this.listUsers();
+    }
+}).mount("#song");
---
+++ src/main/resources/client/js/song_type.js
@@ -0,0 +1,15 @@
+new Vue({
+    el: '#app',
+    data: {
+        categories: [
+            { name: '全部歌单', url: '#' },
+            { name: '华语', url: '#' },
+            { name: '粤语', url: '#' },
+            { name: '欧美', url: '#' },
+            { name: '日韩', url: '#' },
+            { name: '轻音乐', url: '#' },
+            { name: 'BGM', url: '#' },
+            { name: '乐器', url: '#' }
+        ]
+    }
+})
