c79ea76d36db920bdf44975366abba8b97d172c5
mikudd3 <488607948@qq.com>
2023-08-16 16:56:42 +0800

    首页

---
+++ src/main/resources/client/page/main/index.js
@@ -0,0 +1,108 @@
+new Vue({
+    el: '#slider',
+    data() {
+        return {
+            slides: [
+                {url: '../../image/a (1).png'},
+                {url: '../../image/a (2).png'},
+            ],
+            currentIndex: 0,
+            timerId: null
+        };
+    },
+    computed: {
+        currentSlide() {
+            return this.slides[this.currentIndex];
+        }
+    },
+    mounted() {
+        // this.startAutoPlay();
+        this.fetchSlides();
+    },
+    methods: {
+        fetchSlides() {
+            axios({
+                url: "/house/getlbHouse",
+                method: "get"
+            }).then(response => {
+                if (response.data.code == 1) {
+                    let r = response.data;
+                    this.slides = r.data;
+                    //进行图片处理
+                    for (let i = 0; i < this.slides.length; i++) {
+                        this.slides[i].url = `/common/download?name=${this.slides[i].url}`
+                    }
+                    console.log(r.data)
+                    this.startAutoPlay();
+                } else {
+                    console.log(response)
+                    this.$message.error(response.data.msg)
+                }
+            }).catch(error => {
+                console.error('Error fetching slides:', error);
+            });
+        },
+        nextSlide() {
+            this.currentIndex = (this.currentIndex + 1) % this.slides.length;
+        },
+        prevSlide() {
+            this.currentIndex = (this.currentIndex - 1 + this.slides.length) % this.slides.length;
+        },
+        startAutoPlay() {
+            this.timerId = setInterval(() => {
+                this.nextSlide();
+            }, 1500);
+        },
+        stopAutoPlay() {
+            clearInterval(this.timerId);
+        }
+    },
+    beforeDestroy() {
+        this.stopAutoPlay();
+    }
+})
+
+
+new Vue({
+    el: "#gedan",
+    data() {
+        return {
+            songs: [
+                {
+                    id: 1,
+                    imageUrl: 'https://liquan-springboot-music.oss-cn-shanghai.aliyuncs.com/images/introduce_img/f1.jpg',
+                    title: '每日最新单曲',
+                    playCount: '640万'
+                },
+                {
+                    id: 2,
+                    imageUrl: 'https://liquan-springboot-music.oss-cn-shanghai.aliyuncs.com/images/introduce_img/f13.jpg',
+                    title: '每日最新网络单曲',
+                    playCount: '514万'
+                },
+                {
+                    id: 3,
+                    imageUrl: 'https://liquan-springboot-music.oss-cn-shanghai.aliyuncs.com/images/introduce_img/z25.jpg',
+                    title: '最新伤感情歌',
+                    playCount: '960万'
+                },
+                {
+                    id: 4,
+                    imageUrl: 'https://liquan-springboot-music.oss-cn-shanghai.aliyuncs.com/images/introduce_img/f12.jpg',
+                    title: '最新古风歌曲',
+                    playCount: '351万'
+                },
+                {
+                    id: 5,
+                    imageUrl: 'https://liquan-springboot-music.oss-cn-shanghai.aliyuncs.com/images/introduce_img/f12.jpg',
+                    title: '最新古风歌曲',
+                    playCount: '351万'
+                },
+
+            ]
+        };
+    },
+    methods() {
+
+    }
+})
