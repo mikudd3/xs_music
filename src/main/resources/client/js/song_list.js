d1b5970804661a601363b61a157f9f5e80fa269f
cbj <2789531926@qq.com>
2023-08-17 09:01:43 +0800

    歌单页面变更

--- src/main/resources/client/js/song_list.js
+++ src/main/resources/client/js/song_list.js
@@ -1,6 +1,8 @@
-Vue.createApp({
+new Vue({
+    el: '#m3',
     data(){
         return {
+            categories: [],
             song_list: [],
             title:"",
             pic:"",
@@ -10,7 +12,21 @@ Vue.createApp({
             create_time:""
         }
     },
-
+    created() {
+        // 调用API或其他方式获取categories数据
+        // 假设获取到的数据是一个数组
+        this.categories=[
+            { name: '全部歌单', url: '#' },
+            { name: '华语', url: '#' },
+            { name: '粤语', url: '#' },
+            { name: '欧美', url: '#' },
+            { name: '日韩', url: '#' },
+            { name: '轻音乐', url: '#' },
+            { name: 'BGM', url: '#' },
+            { name: '乐器', url: '#' }];
+        this.pic="../../image/a(1).png";
+        this.introduction="11111";
+    },
     methods:{
         listUsers(){
             axios({
@@ -25,7 +41,7 @@ Vue.createApp({
                     create_time: this.create_time
                 }
             }).then(res => {
-                this.song_list = res.song_list;
+                this.song_list = res.data;
             })
         },
 
@@ -33,4 +49,4 @@ Vue.createApp({
     mounted(){
         this.listUsers();
     }
-}).mount("#song");
+})
