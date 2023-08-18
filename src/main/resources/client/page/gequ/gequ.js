a46b7ca7c9563761f778c86c26470c0337da89f1
yanguanghesama <1772133828@11>
2023-08-18 20:36:20 +0800

    我喜欢

--- src/main/resources/client/page/gequ/gequ.js
+++ src/main/resources/client/page/gequ/gequ.js
@@ -1,14 +1,42 @@
-const likeButtons = document.querySelectorAll(".likeButton");
+new Vue({
+    el: "#list",
+    data: {
+        listResout:[
+            {
+                //必要信息
+                ImgUrl: 'http://www.170hi.com/wp-content/themes/beetube/images/nopic.png',
+                song: 'https://www.ihaoge.net/server/1/287280938.mp3',
+                name: '叮叮当',
+                singer: '宝宝巴士',
+                like:true
+            },
+            {
+                ImgUrl: 'https://star.kuwo.cn/star/starheads/180/21/12/1142472669.jpg',
+                song: 'https://www.ihaoge.net/server/1/283424829.mp3',
+                name: '水中花(Live)',
+                singer: '郁可唯',
+                like:false
+            },
+            {
+                ImgUrl: 'http://static.170hi.com/wp-content/themes/beetube/images/nopic.png',
+                song: 'https://www.ihaoge.net/server/1/289548192.mp3',
+                name: '僕らのスペクトラ',
+                singer: '北谷洋',
+                like:false
+            }
+        ],
 
-likeButtons.forEach(function(likeButton) {
-    likeButton.addEventListener("click", function () {
-        this.querySelector(".heart-icon").classList.toggle("active");
-
-        // 根据点赞状态执行相应的操作
-        if (this.querySelector(".heart-icon").classList.contains("active")) {
-            // 执行点赞操作
-        } else {
-            // 执行取消点赞操作
+    },
+    methods: {
+        //喜欢
+        toggleFavorite(row) {
+            row.like = !row.like;
+            //添加进我喜欢的请求
+        },
+        //播放
+        addtoplay(row){
+            //信息塞进session域中
         }
-    });
+
+    }
 });
