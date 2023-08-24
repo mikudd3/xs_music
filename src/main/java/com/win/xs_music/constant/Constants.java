5e8e181d31a2c16765eac52a18e65a7108ab239b
mikudd3 <488607948@qq.com>
2023-08-24 20:12:51 +0800

    bug

--- src/main/java/com/win/xs_music/constant/Constants.java
+++ src/main/java/com/win/xs_music/constant/Constants.java
@@ -7,7 +7,7 @@ public class Constants {
     public static String AVATOR_IMAGES_PATH = "file:" + ASSETS_PATH + "/img/avatorImages/";
     public static String SONGLIST_PIC_PATH = "file:" + ASSETS_PATH + "/img/songListPic/";
     public static String SONG_PIC_PATH = "file:" + ASSETS_PATH + "/img/songPic/";
-    public static String SONG_PATH = "file:" + ASSETS_PATH + "/song.html/";
+    public static String SONG_PATH = "file:" + ASSETS_PATH + "/song/";
     public static String SINGER_PIC_PATH = "file:" + ASSETS_PATH + "/img/singerPic/";
     public static String BANNER_PIC_PATH = "file:" + ASSETS_PATH + "/img/swiper/";
 
